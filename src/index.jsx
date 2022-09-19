1e996a1696242fa22a16543c8a86ddd012d92426
Morley Tatro <mtatro@figure.com>
2022-09-19 14:16:32 -0700

    wrap with StrictMode component

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,9 @@ import './style.css';
 const root = createRoot(document.querySelector('#app'));
 
 root.render(
-  <div className="bg-blue-500 text-white p-4">
-    Hello reloaded!
-  </div>
+  <React.StrictMode>
+    <div className="bg-blue-500 text-white p-4">
+      Hello reloaded!
+    </div>
+  </React.StrictMode>
 );
